9039bfc2d331e812edf161c251e6a7b748542047
psamala99 <97566503+psamala99@users.noreply.github.com>
2023-03-31 22:52:14 -0400

    Add files via upload

---
+++ Code/fixing_transformer.py
@@ -0,0 +1,229 @@
+# The following requires torch v0.12+ and torchvision v0.13+
+import torch
+import torchvision
+from torchinfo import summary
+from torch import nn
+import data_setup
+import engine
+from helper_functions import set_seeds
+from helper_functions import plot_loss_curves, pred_and_plot_image
+import sys
+sys.path.append('/home/ubuntu/Test_Capstone/pytorch-image-models/timm')
+import timm
+
+
+
+from PIL import Image
+from data_setup import create_dataloaders
+from torchvision.transforms import Grayscale
+print(torch.__version__)
+print(torchvision.__version__)
+
+#-----------------------------------------------------------------------------------------------------------------------
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+
+model = timm.create_model('vit_base_patch16_224', pretrained=True).to(device)
+#custom_model = VisionTransformer()
+#custom_model.to(device)
+# Update positional embeddings
+weights = model.patch_embed.proj.weight
+new_weights = weights[:, :1, ...]
+model.patch_embed.proj.weight = torch.nn.Parameter(new_weights)
+
+# Update positional embeddings
+
+
+# 3. Freeze the base parameters
+for parameter in model.parameters():
+    parameter.requires_grad = False
+
+# 4. Change the classifier head (set the seeds to ensure same initialization with linear head)
+set_seeds()
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+# Linear head
+#model.head = nn.Linear(in_features=768, out_features=10).to(device)
+
+
+# MLP head
+'''model.head = nn.Sequential(
+            nn.Linear(in_features=768, out_features=256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(in_features=256, out_features=10))'''
+
+# MLP Mixer
+class MLP_mixer_Head(nn.Module):
+    def __init__(self):
+        super(MLP_mixer_Head, self).__init__()
+        self.fc1 = nn.Linear(768, 256)
+        self.ln1 = nn.LayerNorm(256)
+        self.fc2 = nn.Linear(256, 100)
+        self.ln2 = nn.LayerNorm(100)
+        #self.fc3 = nn.Linear(1344,1152)
+        #self.fc4 = nn.Linear(1152, 968)
+        self.fc5 = nn.Linear(100, 10)
+        self.gelu = nn.GELU()
+        self.drop = nn.Dropout(0.2)
+
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.ln1(x)
+        x = self.gelu(x)
+        x = self.drop(x)
+
+        x = self.fc2(x)
+        x = self.ln2(x)
+        x = self.gelu(x)
+        x = self.drop(x)
+
+        '''x = self.fc3(x)
+        x = self.gelu(x)
+        x = self.drop(x)
+
+        x = self.fc4(x)
+        x = self.gelu(x)
+        x = self.drop(x)'''
+
+        x = self.fc5(x)
+        x = self.drop(x)
+        return x
+
+
+mlp_mixer_head = MLP_mixer_Head()
+
+
+
+# CNN head
+class CNNHead(nn.Module):
+    def __init__(self):
+        super(CNNHead, self).__init__()
+        self.conv1 = nn.Conv2d(768, 576, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(576, 380, kernel_size=3, stride=1, padding=1)
+
+
+        self.conv3 = nn.Conv2d(380, 160, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(160, 80, kernel_size=3, stride=1, padding=1)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d((1,1))
+        self.dropout = nn.Dropout(0.25)
+
+        self.fc1 = nn.Linear(80, 10)
+
+    def forward(self, x):
+        x = x.unsqueeze(-1).unsqueeze(-1)
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.dropout(x)
+
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.dropout(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
+
+cnn_head = CNNHead()
+
+model.head = cnn_head
+
+#model.head = mlp_mixer_head
+
+
+
+# pretrained_vit # uncomment for model output
+#-----------------------------------------------------------------------------------------------------------------------
+
+# # Print a summary using torchinfo (uncomment for actual output)
+summary(model=model,
+         input_size=(128, 1, 224, 224), # (batch_size, color_channels, height, width)
+         # col_names=["input_size"], # uncomment for smaller output
+         col_names=["input_size", "output_size", "num_params", "trainable"],
+         col_width=20,
+        row_settings=["var_names"])
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+image_path = '/home/ubuntu/Test_Capstone/Data/'
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Setup dirs
+train_dir = '/home/ubuntu/Test_Capstone/Data_Transformer/Train/'
+test_dir = '/home/ubuntu/Test_Capstone/Data_Transformer/Test/'
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Get automatic transforms from pretrained ViT weights
+transforms = torchvision.transforms.Compose([
+    torchvision.transforms.Grayscale(num_output_channels=1),
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=[0.485],std=[0.229])])
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Setup dataloaders
+train_dataloader_pretrained, test_dataloader_pretrained, class_names = data_setup.create_dataloaders(train_dir=train_dir,
+        test_dir=test_dir, transform=transforms, batch_size=128)
+
+print(class_names)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# Create optimizer and loss function
+optimizer = torch.optim.Adam(params=model.parameters(),lr=1e-3)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+
+# Train the classifier head of the pretrained ViT feature extractor model
+set_seeds()
+pretrained_vit_results = engine.train(model=model,
+                                      train_dataloader=train_dataloader_pretrained,
+                                      test_dataloader=test_dataloader_pretrained,
+                                      optimizer=optimizer,
+                                      loss_fn=loss_fn,
+                                      epochs=20,
+                                      device=device)
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+plot_loss_curves(pretrained_vit_results)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+pred_transforms=torchvision.transforms.Compose([
+    torchvision.transforms.Grayscale(num_output_channels=1),
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224)])
+
+predictions = pred_and_plot_image(model=model,
+                            image_path=test_dir + 'Mass/00030594_000.png',
+                            class_names=class_names,
+                            transform=pred_transforms,
+                            device=device)
+
